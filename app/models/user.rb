2378f5b6666218d0f6ef76f4bb1882edf18e6329
marycriv <maryecrivelli@gmail.com>
2019-09-01 14:46:58 -0400

    Seed data edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_many :bitten_users, foreign_key: :biter_id, class_name: 'Bite'
-  has_many :bitees, through: :bitten_users
-  has_many :biting_users, foreign_key: :bitee_id, class_name: 'Bite'
-  has_many :biters, through: :biting_users
+  has_many :bitten_users, foreign_key: :biter_id, class_name: 'Bite', dependent: :destroy
+  has_many :bitees, through: :bitten_users, dependent: :destroy
+  has_many :biting_users, foreign_key: :bitee_id, class_name: 'Bite', dependent: :destroy
+  has_many :biters, through: :biting_users, dependent: :destroy
 
   validates :display_name, uniqueness: true
   validates :classification, inclusion: { in: ["vampire", "dhampir", "slayer", "human"] }
